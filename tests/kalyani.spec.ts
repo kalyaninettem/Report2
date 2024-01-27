be1e5cba38fb30a2fd284f811b07d05bc8054671
Kalyanikonijati <kalyani.konijeti6@gmail.com>
2024-01-27 15:33:21 -0600

    adding test scripts

---
+++ tests/kalyani.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+// get text 58K and print to console.
+test('my practice', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  expect(await page.locator('.DocSearch-Button-Placeholder').isVisible()).toBe(true);
+  await page.locator('.DocSearch-Button-Placeholder').click();
+  await page.waitForTimeout(5000);
+  await page.locator("#docsearch-input").fill('image');
+  await page.locator('#docsearch-item-2').click();
+  //let pageText = await page.locator('').textContent();
+  
+  // await page.locator("[aria-label='58k+ stargazers on GitHub']").click();
+  // Click the get started link.
+  // await page.getByRole('link', { name: 'star' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+//#docsearch-item-2
